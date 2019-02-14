5c688f7052b4cff0ca070193116f5a24fbf051b2
AjdamStupava <46596136+AjdamStupava@users.noreply.github.com>
2019-02-14 12:35:04 +0100

    Add files via upload
    4th commit ... still I don`t really use git technology :/

--- unit1.pas
+++ unit1.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
-  ExtCtrls;
+  ExtCtrls, EditBtn;
 
 type zoznam=record
   nazov,kod:String;
@@ -17,10 +17,10 @@ end;
 
   TForm1 = class(TForm)
     Button1: TButton;
-    Edit1: TEdit;
     Edit2: TEdit;
     Edit3: TEdit;
-    Edit4: TEdit;
+    EditButton1: TEditButton;
+    EditButton2: TEditButton;
     Image1: TImage;
     Label1: TLabel;
     Label2: TLabel;
@@ -38,14 +38,15 @@ end;
     Memo1: TMemo;
     Timer1: TTimer;
     procedure Button1Click(Sender: TObject);
-    procedure Edit1EditingDone(Sender: TObject);
-    procedure Edit4EditingDone(Sender: TObject);
+    procedure EditButton1ButtonClick(Sender: TObject);
+    procedure EditButton2ButtonClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure ListBox1SelectionChange(Sender: TObject; User: boolean);
     procedure ListBox2SelectionChange(Sender: TObject; User: boolean);
     procedure ListBox3SelectionChange(Sender: TObject; User: boolean);
     procedure ListBox4SelectionChange(Sender: TObject; User: boolean);
     procedure aktualizuj;
+    procedure twodecplaces(i:Integer);
   private
     { private declarations }
   public
@@ -74,8 +75,7 @@ begin
 
   Memo1.Clear;
 
-
-  AssignFile(subor,'TOVAR.txt');
+  AssignFile(subor,'TOVAR.txt');     // nacitavanie kodov a nazvov tovarov
   Reset(subor);
   Readln(subor,pocet);
   i:=0;
@@ -89,10 +89,10 @@ begin
     cennik[i].nazov:=COPY(pomoc,poz+1,length(pomoc));
    end;
 
-  CloseFile(subor);
+  CloseFile(subor);            // koniec nacitavania kodov a nazvov tovarov
 
 
-  AssignFile(subor2,'CENNIK.txt');
+  AssignFile(subor2,'CENNIK.txt');    // nacitavanie cien
   Reset(subor2);
   Readln(subor2);
   i:=0;
@@ -108,14 +108,13 @@ begin
     cennik[i].predaj:=StrToFloat(COPY(pomoc,poz+1,length(pomoc))) / 100;
    end;
 
-  CloseFile(subor2);
+  CloseFile(subor2);                // koniec nacitavania cien
 
 
   for i:=1 to pocet do
    begin
     ListBox1.Items.Add(cennik[i].nazov);
-    ListBox2.Items.Add(FloatToStr(cennik[i].nakup)+' €');
-    ListBox3.Items.Add(FloatToStr(cennik[i].predaj)+' €');
+    twodecplaces(i);                       // vypis cien do listboxov
     ListBox4.Items.Add(cennik[i].kod);
    end;
 
@@ -129,10 +128,10 @@ begin
    IF ListBox1.Selected[i] THEN
     begin
       aktual:=i+1;
-      Edit1.text:=cennik[i+1].nazov;
+      EditButton2.text:=cennik[i+1].nazov;
       Edit2.text:=FloatToStr(cennik[i+1].nakup);
       Edit3.text:=FloatToStr(cennik[i+1].predaj);
-      Edit4.text:=cennik[i+1].kod;
+      EditButton1.text:=cennik[i+1].kod;
       ListBox2.ItemIndex:=i;
       ListBox3.ItemIndex:=i;
       ListBox4.ItemIndex:=i;
@@ -148,10 +147,10 @@ begin
    IF ListBox2.Selected[i] THEN
     begin
       aktual:=i+1;
-      Edit1.text:=cennik[i+1].nazov;
+      EditButton2.text:=cennik[i+1].nazov;
       Edit2.text:=FloatToStr(cennik[i+1].nakup);
       Edit3.text:=FloatToStr(cennik[i+1].predaj);
-      Edit4.text:=cennik[i+1].kod;
+      EditButton1.text:=cennik[i+1].kod;
       ListBox1.ItemIndex:=i;
       ListBox3.ItemIndex:=i;
       ListBox4.ItemIndex:=i;
@@ -167,10 +166,10 @@ begin
    IF ListBox3.Selected[i] THEN
     begin
       aktual:=i+1;
-      Edit1.text:=cennik[i+1].nazov;
+      EditButton2.text:=cennik[i+1].nazov;
       Edit2.text:=FloatToStr(cennik[i+1].nakup);
       Edit3.text:=FloatToStr(cennik[i+1].predaj);
-      Edit4.text:=cennik[i+1].kod;
+      EditButton1.text:=cennik[i+1].kod;
       ListBox1.ItemIndex:=i;
       ListBox2.ItemIndex:=i;
       ListBox4.ItemIndex:=i;
@@ -186,55 +185,109 @@ begin
    IF ListBox4.Selected[i] THEN
     begin
       aktual:=i+1;
-      Edit1.text:=cennik[i+1].nazov;
+      EditButton2.text:=cennik[i+1].nazov;
       Edit2.text:=FloatToStr(cennik[i+1].nakup);
       Edit3.text:=FloatToStr(cennik[i+1].predaj);
-      Edit4.text:=cennik[i+1].kod;
+      EditButton1.text:=cennik[i+1].kod;
       ListBox1.ItemIndex:=i;
       ListBox2.ItemIndex:=i;
-      listBox3.ItemIndex:=i;
+      ListBox3.ItemIndex:=i;
     end;
 
 end;
 
-procedure TForm1.Edit1EditingDone(Sender: TObject);
-var i:Integer;
+procedure TForm1.EditButton1ButtonClick(Sender: TObject);
+var i,k:Integer;
 begin
 
   for i:=1 to pocet do
-   IF Edit1.text = cennik[i].nazov THEN
+   IF EditButton1.text = cennik[i].kod THEN
     begin
+     k:=1;
      aktual:=i;
+     EditButton2.text:=cennik[i].nazov;
      Edit2.text:=FloatToStr(cennik[i].nakup);
      Edit3.text:=FloatToStr(cennik[i].predaj);
-     Edit4.text:=cennik[i].kod;
-    end;
+     ListBox1.ItemIndex:=i-1;
+     ListBox2.ItemIndex:=i-1;
+     ListBox3.ItemIndex:=i-1;
+     ListBox4.ItemIndex:=i-1;
+     exit;
+    end
+   ELSE
+    k:=0;
+
+  IF k=0 THEN
+   begin
+    ShowMessage('Tovar s týmto kódom sa nenachádza na sklade.');
+   end;
 
 end;
 
-procedure TForm1.Edit4EditingDone(Sender: TObject);
-var i:Integer;
+procedure TForm1.EditButton2ButtonClick(Sender: TObject);
+var i,k:Integer;
 begin
 
   for i:=1 to pocet do
-   IF Edit4.text = cennik[i].kod THEN
+   IF EditButton2.text = cennik[i].nazov THEN
     begin
+     k:=1;
      aktual:=i;
-     Edit1.text:=cennik[i].nazov;
+     EditButton1.text:=cennik[i].kod;
      Edit2.text:=FloatToStr(cennik[i].nakup);
      Edit3.text:=FloatToStr(cennik[i].predaj);
-    end;
+     ListBox1.ItemIndex:=i-1;
+     ListBox2.ItemIndex:=i-1;
+     ListBox3.ItemIndex:=i-1;
+     ListBox4.ItemIndex:=i-1;
+     exit;
+    end
+   ELSE
+    k:=0;
+
+  IF k=0 THEN
+   begin
+    ShowMessage('Tento tovar sa nenachádza na sklade.');
+   end;
 
 end;
 
 procedure TForm1.Button1Click(Sender: TObject);
+var kontrola,check:Boolean;
+    zlomok:Real;
 begin
   Memo1.Clear;
 
+
+  kontrola:=TryStrToFloat(Edit2.text,zlomok);
+  IF not kontrola THEN
+   begin
+    Edit2.text:=FloatToStr(cennik[aktual].nakup);
+    Edit3.text:=FloatToStr(cennik[aktual].predaj);
+    ShowMessage('Zadajte nákupnú cenu tovaru.');
+   end;
+
+  check:=TryStrToFloat(Edit3.text,zlomok);
+  IF not check THEN
+   begin
+    Edit2.text:=FloatToStr(cennik[aktual].nakup);
+    Edit3.text:=FloatToStr(cennik[aktual].predaj);
+    ShowMessage('Zadajte predajnú cenu tovaru.');
+   end;
+
+  IF (StrToFloat(Edit2.text)<=0) or (StrToFloat(Edit3.text)<=0) THEN
+   begin
+    Edit2.text:=FloatToStr(cennik[aktual].nakup);
+    Edit3.text:=FloatToStr(cennik[aktual].predaj);
+    ShowMessage('Zadajte cenu tovaru, zadarmo nekupujeme ani nepredávame.');
+   end;
+
+
+  IF (check=true) and (kontrola=true) and (StrToFloat(Edit2.text)>0) and (StrToFloat(Edit3.text)>0) THEN
     IF StrToFloat(Edit2.text) > StrToFloat(Edit3.text) THEN
      begin
 
-       IF MessageDlg('Otázka', 'Chcete zadať nákupnú cenu vyššiu ako predajnú?', mtConfirmation,
+       IF MessageDlg('Otázka', 'Chcete uložiť nákupnú cenu vyššiu ako predajnú?', mtConfirmation,
        [mbYes, mbNo],0) = mrYes THEN
           aktualizuj
        ELSE
@@ -249,16 +302,26 @@ end;
 procedure TForm1.aktualizuj;
 var i:Integer;
 begin
+
   cennik[aktual].nakup:=StrToFloat(Edit2.text);
   cennik[aktual].predaj:=StrToFloat(Edit3.text);
   ListBox2.Items.Clear;
   ListBox3.Items.Clear;
 
   for i:=1 to pocet do
+    twodecplaces(i);
+
+  AssignFile(subor2,'CENNIK.txt');
+  Rewrite(subor2);
+  Writeln(subor2,IntToStr(pocet));
+
+  for i:=1 to pocet do
    begin
-    ListBox2.Items.Add(FloatToStr(cennik[i].nakup)+' €');
-    ListBox3.Items.Add(FloatToStr(cennik[i].predaj)+' €');
+    Writeln(subor2,cennik[i].kod+';'+FloatToStr(cennik[i].nakup*100)+';'+FloatToStr(cennik[i].predaj*100));
    end;
+
+  CloseFile(subor2);
+
 
   Memo1.Append(cennik[aktual].kod);
   Memo1.Append(cennik[aktual].nazov);
@@ -267,5 +330,36 @@ begin
 
 end;
 
+procedure TForm1.twodecplaces(i:Integer);
+var ncena,pcena:String;
+begin
+  ncena:=FloatToStr(cennik[i].nakup*100);
+
+    IF (ncena[length(ncena)] = '0') THEN
+     begin
+      IF (ncena[length(ncena)-1] = '0') THEN
+        ListBox2.Items.Add(FloatToStr(cennik[i].nakup)+',00 €')
+      ELSE
+        ListBox2.Items.Add(FloatToStr(cennik[i].nakup)+'0 €');
+     end;
+
+    IF (ncena[length(ncena)] <> '0') THEN
+      ListBox2.Items.Add(FloatToStr(cennik[i].nakup)+' €');
+
+
+  pcena:=FloatToStr(cennik[i].predaj*100);
+
+    IF (pcena[length(pcena)] = '0') THEN
+     begin
+      IF (pcena[length(pcena)-1] = '0') THEN
+        ListBox3.Items.Add(FloatToStr(cennik[i].predaj)+',00 €')
+      ELSE
+        ListBox3.Items.Add(FloatToStr(cennik[i].predaj)+'0 €');
+     end;
+
+    IF (pcena[length(pcena)] <> '0') THEN
+      ListBox3.Items.Add(FloatToStr(cennik[i].predaj)+' €');
+
+end;
 end.
 
